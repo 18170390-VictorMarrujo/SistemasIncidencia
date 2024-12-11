ef528430e6702688b34882a854354bd0fb799edc
Victor Adan Marrujo Carrillo <18170390@itculiacan.edu.mx>
2024-12-10 17:16:50 -0700

    carga de proyecto

--- Incidencias/eliminar_usuario.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'config.php';
-
-$id = $_GET['id'];
-$stmt = $conn->prepare("DELETE FROM usuarios WHERE id_usuario = ?");
-$stmt->execute([$id]);
-
-header("Location: listar_usuarios.php");
-?>
